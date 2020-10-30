644421b2c309983f252154dc4b77f1a2e7991782
Justin Myers <justin@cloudpaper.com>
2020-10-30 00:15:23 -0700

    Add react prop sorter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,8 @@ import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const obj = { b: true, a: false };
   // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} key={obj.a} />;
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
