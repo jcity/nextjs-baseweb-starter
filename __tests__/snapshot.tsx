138b6b3022c7db2ce3c1b80c6182fdcabf29bcef
Justin Myers <justin@cloudpaper.com>
2020-10-30 01:39:50 -0700

    Make importing a little nicer

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import Index from '@pages/index';
+
+import Index from '~/pages/index';
 
 it('renders homepage unchanged', () => {
   const tree = renderer.create(<Index />).toJSON();
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/app';
-import '../styles/globals.css';
+
+import '~/styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   // eslint-disable-next-line react/jsx-props-no-spreading
