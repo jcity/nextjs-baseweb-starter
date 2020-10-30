171461dd661c1707c8ac085eef42ede690581146
Justin Myers <justin@cloudpaper.com>
2020-10-29 22:42:00 -0700

    Configure eslint and prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/app';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppProps) {
+  const obj = { b: true, a: false };
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} key={obj.a} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,8 +1,9 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextApiRequest, NextApiResponse } from 'next';
 
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default (req: NextApiRequest, res: NextApiResponse): void => {
+  const obj = { b: true, a: false };
   res.statusCode = 200;
-  res.json({ name: "John Doe" });
+  res.json({ name: 'John Doe' });
 };
